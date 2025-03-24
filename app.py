18b46965a360c141da8f6af373430e4a464c35ca
Your Name <gaikwad2udit@gmail.com>
2025-03-24 17:23:46 +0530

    --

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ class User( db.Model):
         return f'<User {self.username}>'
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash("mypassword", method="pbkdf2:sha256", salt_length=8, iterations=100000)
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
